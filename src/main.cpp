e20e4016996101cd58444c31017e486d29cdbc81
jim <jimorcheson@gmail.com>
2022-07-06 21:12:59 -0400

    Refactor printing of strings

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,27 @@
 
 using namespace SoapySDR;
 
+class Print
+{
+    public:
+        static void printStrings(const std::vector<std::string>& strings, 
+                            const uint indentSpaces)
+        {
+            std::string indent(indentSpaces, ' ');
+            if(strings.size() == 0)
+            {
+                std::cout << indent << "None\n";
+            }
+            else
+            {
+                for(const auto& format : strings)
+                {
+                    std::cout << indent << format << '\n';
+                } 
+            }
+        }
+};
+
 int main()
 {
     std::vector<Kwargs> kList = Device::enumerate();
@@ -52,19 +73,9 @@ int main()
                     std::cout << "    " << key << ": " << channelInfo[key] << '\n';
                 }
             }
-            auto streamFormats = device->getStreamFormats(SOAPY_SDR_RX, channel);
+            auto rxStreamFormats = device->getStreamFormats(SOAPY_SDR_RX, channel);
             std::cout << "    RX Stream Formats:\n";
-            if(streamFormats.size() == 0)
-            {
-                std::cout << "        None\n";
-            }
-            else
-            {
-                for(const auto& format : streamFormats)
-                {
-                    std::cout << "        " << format << '\n';
-                } 
-            }
+            Print::printStrings(rxStreamFormats, 8);
         }
 
         const auto& txFrontendMapping = device->getFrontendMapping(SOAPY_SDR_TX);
@@ -91,17 +102,7 @@ int main()
             }
             auto txStreamFormats = device->getStreamFormats(SOAPY_SDR_TX, channel);
             std::cout << "    TX Stream Formats:\n";
-            if(txStreamFormats.size() == 0)
-            {
-                std::cout << "        None\n";
-            }
-            else
-            {
-                for(const auto& format : txStreamFormats)
-                {
-                    std::cout << "        " << format << '\n';
-                } 
-            }
+            Print::printStrings(txStreamFormats, 8);
         }
 
         Device::unmake(device);
